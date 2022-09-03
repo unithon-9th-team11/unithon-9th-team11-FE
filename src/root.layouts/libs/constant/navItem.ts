2a1a33127a0eb0e7f20d81f025a4d2101398b4c0
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-09-03 20:16:48 +0900

    네브바 수정

--- src/root.layouts/libs/constant/navItem.ts
+++ src/root.layouts/libs/constant/navItem.ts
@@ -6,14 +6,12 @@ export type navItemType = {
 // TODO 개선
 export const logOutNavItems: navItemType[] = [
   { title: '궁합보기', path: '/' },
-  { title: '랭킹조회', path: '/' },
   { title: '로그인', path: '/' },
   { title: '회원가입', path: '/' },
 ];
 
 export const loginNavItems: navItemType[] = [
   { title: '궁합보기', path: '/' },
-  { title: '랭킹조회', path: '/' },
-  { title: '프로필보기??', path: '/' },
+  { title: '마이랭킹', path: '/' },
   { title: '로그아웃', path: '/' },
 ];
