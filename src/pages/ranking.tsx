220e3fdbd824c8c4dae815327d1498bf3032ac52
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-09-03 21:44:25 +0900

    axios

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -1,17 +1,44 @@
 import UsersLayout from '@RootComponents/UsersLayout';
 import Link from 'next/link';
+import axios from 'axios';
 
-export interface usergit {
-    name : string;
-}
+type Usergit = {
+  name: string;
+};
+
+type GetUsergitResponse = {
+  data: Usergit[];
+};
 
 const Ranking = () => {
-    const [usergit, setuserGie] = 
+    const getUsergit = async()=>{
+        try {
+            const{data} = await axios.get<GetUsergitResponse>(
+                'api 주소', //api 주소 여기다가 넣기
+                {
+                    headers : {
+                        Accept: 'application/json',
+                    },
+                },
+            );
+            console.log(JSON.stringify(data, null, 2));
+            return data;
+        } catch (error){
+            if(axios.isAxiosError(error)){
+                console.log('error message: ', error.message);
+                return error.message;
+            }else{
+                console.log('unexpected error: ', error);
+                return '이거는 unexpected error';
+            }
+        }
+    }
+    getUsergit();
   return (
-    <UsersLayout>
-        
-    </UsersLayout>
+  <UsersLayout>
+      
+  </UsersLayout>
   )
-}
+};
 
-export default Ranking
+export default Ranking;
