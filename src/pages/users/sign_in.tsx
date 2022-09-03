01b9020ed4686e8c10fc5a809ef0d7fcd31771d9
gamrom <gamrom01@naver.com>
2022-09-03 16:00:16 +0900

    로그인 OK

---
+++ src/pages/users/sign_in.tsx
@@ -0,0 +1,76 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import 'antd/dist/antd.css';
+import { Button, Input } from 'antd';
+import * as Yup from 'yup';
+import UsersLayout from '@RootComponents/UsersLayout';
+
+type SignInValue = {
+  userId: string;
+  userPw: string;
+};
+
+const SignIn = () => {
+  const validationSchema = Yup.object().shape({
+    userId: Yup.string()
+      .email('이메일 형식으로 입력해주세요.')
+      .required('이메일을 입력해주세요.'),
+    userPw: Yup.string().required('비밀번호를 입력해주세요.'),
+  });
+
+  return (
+    <UsersLayout>
+      <Formik
+        initialValues={{
+          userId: '',
+          userPw: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values: SignInValue) => {
+          console.log(values.userId);
+          console.log(values.userPw);
+        }}
+      >
+        <Form>
+          <Field name="userId">
+            {({ field }) => {
+              return (
+                <Input
+                  {...field}
+                  className="input-style"
+                  type="email"
+                  autoComplete="false"
+                />
+              );
+            }}
+          </Field>
+
+          <div className="validation-msg">
+            <ErrorMessage name="userId" />
+          </div>
+
+          <Field name="userPw">
+            {({ field }) => {
+              return (
+                <Input.Password
+                  {...field}
+                  className="input-style"
+                  autoComplete="false"
+                />
+              );
+            }}
+          </Field>
+
+          <div className="validation-msg">
+            <ErrorMessage name="userPw" />
+          </div>
+
+          <Button htmlType="submit" className="btn-submit">
+            로그인
+          </Button>
+        </Form>
+      </Formik>
+    </UsersLayout>
+  );
+};
+
+export default SignIn;
---
+++ src/root.components/UsersLayout.tsx
@@ -0,0 +1,53 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+const UsersLayout = ({ children }: PropsWithChildren<{}>) => {
+  return (
+    <StyledWrapper>
+      <div className="content-style">{children}</div>
+    </StyledWrapper>
+  );
+};
+
+export default UsersLayout;
+
+const StyledWrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+
+  .input-style {
+    margin: 10px 0;
+    height: 45px;
+    border-radius: 14px;
+  }
+
+  .btn-submit {
+    width: 100%;
+    background-color: #f21361;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-weight: bold;
+    height: 45px;
+    border-radius: 14px;
+    margin-top: 15px;
+  }
+
+  .content-style {
+    max-width: 500px;
+    width: 100%;
+    margin-right: auto;
+    margin-left: auto;
+  }
+
+  .validation-msg {
+    font-size: 14px;
+    color: red;
+    margin-top: 8px;
+    margin-bottom: 8px;
+  }
+`;
