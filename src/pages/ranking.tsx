16a8ea5476ecbd1a5137810d572aae27b4b90dc5
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-09-04 03:21:09 +0900

    컴포넌트 map 함수

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -14,53 +14,58 @@ type GetUsergitResponse = {
 
 const Ranking = () => {
   //axios로 데이터 get 해오기
-  // const getUsergit = async()=>{
-  //     try {
-  //         const{data} = await axios.get<GetUsergitResponse>(
-  //             'api 주소', //api 주소 여기다가 넣기
-  //             {
-  //                 headers : {
-  //                     Accept: 'application/json',
-  //                 },
-  //             },
-  //         );
-  //         console.log(JSON.stringify(data, null, 2));
-  //         return data;
-  //     } catch (error){
-  //         if(axios.isAxiosError(error)){
-  //             console.log('error message: ', error.message);
-  //             return error.message;
-  //         }else{
-  //             console.log('unexpected error: ', error);
-  //             return '이거는 unexpected error';
-  //         }
-  //     }
-  // }
-  // getUsergit();
-
-  // 랭킹데이터 객체
-  const RankingData = ({getUsergit}) => {
-      
-  };
+  const getUsergit = async()=>{
+      try {
+          const{data} = await axios.get<GetUsergitResponse>(
+              'api 주소', //api 주소 여기다가 넣기
+              {
+                  headers : {
+                      Accept: 'application/json',
+                  },
+              },
+          );
+          console.log(JSON.stringify(data, null, 2));
+          return data.data[0].;
+      } catch (error){
+          if(axios.isAxiosError(error)){
+              console.log('error message: ', error.message);
+              return error.message;
+          }else{
+              console.log('unexpected error: ', error);
+              return '이거는 unexpected error';
+          }
+      }
+  }
+  getUsergit();
 
   // 랭킹컴포넌트 컬러
   const tenColor = [
-    "#190025",
-    "#2A1039",
-    "#3C285C",
-    "#4D3F7F",
-    "#5D57A2",
-    "#8060DC",
-    "#B27BFF",
-    "#C99DFF",
-    "#E68DFF",
-    "#F5D7FF",
-  ]
+    '#190025',
+    '#2A1039',
+    '#3C285C',
+    '#4D3F7F',
+    '#5D57A2',
+    '#8060DC',
+    '#B27BFF',
+    '#C99DFF',
+    '#E68DFF',
+    '#F5D7FF',
+  ];
+
+  // 랭킹데이터 컴포넌트 객체
+  const RankShow = () => {
+    return getUsergit.data.map((d, index) => (
+      <RankBox backgroundColor={tenColor[index]} key={index}>
+        <span>{d.index+1}</span>
+        <span>{d.name1} + '❤️' + {d.name2}</span>
+        <span>{d.score} + '점'</span>
+      </RankBox>
+    ));
+  };
 
   return (
     <RankingLayout>
       <RankTitle>나와 찰떡궁합 깃허브는?</RankTitle>
-      <RankBox backgroundColor="#190025" />
     </RankingLayout>
   );
 };
@@ -94,19 +99,16 @@ const RankingLayout = styled.div`
   align-items: center;
 `;
 
+// response?.data?.map((d, index) => {
+//   return (
+//     <mappingWrapper props={index}>
+//       <span>d.index +1</span>
+//       <span>d.name</span>
+//       <span>d.</span>
+//     </mappingWrapper>
+//   );
+// });
 
-
-response?.data?.map((d, index) => {
-  return <mappingWrapper props={index}>
-
-    <span>d.index +1</span>
-    <span>d.name</span>
-    <span>d.</span>
-
-  </mappingWrapper>
-})
-
-const mappingWrapper = styled.div`
-  background-color: tenColor[index];
-
-`
+// const mappingWrapper = styled.div`
+//   background-color: tenColor[index];
+// `;
