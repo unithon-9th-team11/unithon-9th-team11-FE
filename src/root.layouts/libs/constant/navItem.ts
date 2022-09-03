c8e249ee0850f064863c235cf33884bb8b8705b2
KyungHoon Jung <ghoon99@kakao.com>
2022-09-04 04:58:58 +0900

    경로 연결 (#20)

--- src/root.layouts/libs/constant/navItem.ts
+++ src/root.layouts/libs/constant/navItem.ts
@@ -5,13 +5,13 @@ export type navItemType = {
 
 // TODO 개선
 export const logOutNavItems: navItemType[] = [
-  { title: '궁합보기', path: '/' },
-  { title: '로그인', path: '/' },
-  { title: '회원가입', path: '/' },
+  { title: '궁합보기', path: '/result/asd' },
+  { title: '로그인', path: '/users/sign_in' },
+  { title: '회원가입', path: '/users/sign_up' },
 ];
 
 export const loginNavItems: navItemType[] = [
-  { title: '궁합보기', path: '/' },
+  { title: '궁합보기', path: '/result/' },
   { title: '마이랭킹', path: '/ranking' },
   { title: '로그아웃', path: '/' },
 ];
